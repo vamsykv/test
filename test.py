9ab746794be1fc5568fa2b6455cae2661f442886
vamsykv <kvamsy21@gmail.com>
2020-05-20 00:09:36 +0530

    Create test.py
    test python and sonar integration

---
+++ test.py
@@ -0,0 +1,27 @@
+
+# Description			: Tests to see if the directory testdir exists, if not it will create the directory for you
+from __future__ import print_function
+
+import os
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
+def main():
+    CheckDir = input("Enter the name of the directory to check : ")
+    print()
+
+    if os.path.exists(CheckDir):  # Checks if the dir exists
+        print("The directory exists")
+    else:
+        print("No directory found for " + CheckDir)  # Output if no directory
+        print()
+        os.makedirs(CheckDir)  # Creates a new dir for the given name
+        print("Directory created for " + CheckDir)
+
+
+if __name__ == '__main__':
+    main()
